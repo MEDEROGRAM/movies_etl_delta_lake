2ae7439fffa4aaff996432ba0c26c1c57b9a48e0
MEDEROGRAM <medero341@gmail.com>
2026-05-17 14:02:17 -0400

    Add files via upload

---
+++ src/analysis/01.results_movie_genre_language (1).py
@@ -0,0 +1,127 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #### Leer todos los datos requeridos
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/common_functions"
+
+# COMMAND ----------
+
+dbutils.widgets.text("p_file_date", "2024-12-30")
+v_file_date = dbutils.widgets.get("p_file_date")
+
+# COMMAND ----------
+
+movies_df = spark.read.table("movie_silver.movies") \
+    .filter(f"file_date = '{v_file_date}'")
+
+# COMMAND ----------
+
+languages_df = spark.read.table("movie_silver.languages")
+
+# COMMAND ----------
+
+movies_languages_df = spark.read.table("movie_silver.movies_languages") \
+    .filter(f"file_date = '{v_file_date}'")
+
+# COMMAND ----------
+
+genres_df = spark.read.table("movie_silver.genres")
+
+# COMMAND ----------
+
+movies_genres_df = spark.read.table("movie_silver.movies_genres") \
+    .filter(f"file_date = '{v_file_date}'")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Join "languages" y "movies_languages"
+
+# COMMAND ----------
+
+languages_mov_lan_df = languages_df.join(movies_languages_df,
+                                      languages_df.language_id == movies_languages_df.language_id,
+                                      "inner") \
+                                          .select(languages_df.language_name, languages_df.language_id,
+                                                  movies_languages_df.movie_id)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Join "genres" y "movies_genres"
+
+# COMMAND ----------
+
+genres_mov_gen_df = genres_df.join(movies_genres_df,
+                                   genres_df.genre_id == movies_genres_df.genre_id,
+                                   "inner") \
+                                       .select(genres_df.genre_name, genres_df.genre_id,
+                                               movies_genres_df.movie_id)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Join "movies_df", "languages_mov_lan_df" y "genres_mov_gen_df"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 1. Filtrar las peliculas donde su fecha sea mayor o igual a 2000
+
+# COMMAND ----------
+
+movie_filter_df = movies_df.filter(movies_df["year_release_date"] >= 2000)
+
+# COMMAND ----------
+
+results_movies_genres_languages_df = movie_filter_df.join(languages_mov_lan_df,
+                        movie_filter_df.movie_id == languages_mov_lan_df.movie_id,
+                        "inner") \
+                            .join(genres_mov_gen_df,
+                                  movie_filter_df.movie_id == genres_mov_gen_df.movie_id,
+                                  "inner")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 2. Agregar la columna "create_date"
+
+# COMMAND ----------
+
+from pyspark.sql.functions import lit
+
+# COMMAND ----------
+
+results_df = results_movies_genres_languages_df \
+    .select(movie_filter_df.movie_id, "language_id", "genre_id", "title", "duration_time", "vote_average", "release_date", "language_name", "genre_name") \
+    .withColumn("created_date", lit(v_file_date))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 3. Ordenar por la columna "release_date" de manera descendente
+
+# COMMAND ----------
+
+results_order_by_df = results_df.orderBy(results_df.release_date.desc())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Escribir datos en el DataLake en formato "Delta"
+
+# COMMAND ----------
+
+#overwrite_partition("movie_gold", "results_movie_genre_language", "created_date", v_file_date)
+
+# COMMAND ----------
+
+merge_condition = 'tgt.movie_id = src.movie_id AND tgt.language_id = src.language_id AND tgt.genre_id = src.genre_id AND tgt.created_date = src.created_date'
+merge_delta_lake(results_order_by_df, "movie_gold", "results_movie_genre_language", merge_condition, "created_date")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from movie_gold.results_movie_genre_language;
